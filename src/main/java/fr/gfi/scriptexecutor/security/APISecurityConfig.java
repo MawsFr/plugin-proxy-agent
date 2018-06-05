81765367fe91af2f2f878ef5733aace4409d6d8a
frus68783 <frus68783@GFI520007.annuaire.groupe.local>
2018-06-05 16:49:20 +0200

    Adding spring security
    - Modifying pom
    - Adding spring security configuration

---
+++ src/main/java/fr/gfi/scriptexecutor/security/APISecurityConfig.java
@@ -0,0 +1,46 @@
+package fr.gfi.scriptexecutor.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+@Configuration
+@EnableWebSecurity
+@Order(1)
+
+public class APISecurityConfig extends WebSecurityConfigurerAdapter {
+	@Value("${auth.header}")
+	private String principalRequestHeader;
+
+	@Value("${auth.token}")
+	private String principalRequestValue;
+
+	@Override
+	protected void configure(final HttpSecurity httpSecurity) throws Exception {
+		final APIKeyAuthFilter filter = new APIKeyAuthFilter(principalRequestHeader);
+		filter.setAuthenticationManager(new AuthenticationManager() {
+
+			@Override
+			public Authentication authenticate(final Authentication authentication) throws AuthenticationException {
+				final String principal = (String) authentication.getPrincipal();
+				if (!principalRequestValue.equals(principal)) {
+					throw new BadCredentialsException("You must specify a valid token in header.");
+				}
+				authentication.setAuthenticated(true);
+				return authentication;
+			}
+		});
+		httpSecurity.antMatcher("/**").csrf().disable().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().addFilter(filter).authorizeRequests()
+				.anyRequest().authenticated();
+	}
+
+}
